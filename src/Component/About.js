db9c5b579d8d534988161647b9bc4cf79140a3e3
kantiboyinaAjay <ajaykantiboyina12389@gmail.com>
2024-08-31 16:06:15 +0530

    removing frontend tag

--- src/Component/About.js
+++ src/Component/About.js
@@ -19,7 +19,7 @@ const About = () => {
         <div className="section__text">
           <p className="section__text__p1">Hello, I'm</p>
           <h1 className="title">Ajay Kantiboyina</h1>
-          <p className="section__text__p2">Frontend Developer</p>
+          {/* <p className="section__text__p2">Frontend Developer</p> */}
           <div className="btn-container">
             <button
               className="btn btn-color-2"
