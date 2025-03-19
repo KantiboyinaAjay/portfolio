61c717722950fd58acd65a9a5fb53688859fd486
kantiboyinaAjay <ajaykantiboyina12389@gmail.com>
2025-03-19 10:21:12 +0530

    update drive link

--- src/Component/About.js
+++ src/Component/About.js
@@ -23,7 +23,7 @@ const About = () => {
           <div className="btn-container">
             <button
               className="btn btn-color-2"
-              onClick={() => window.open('https://drive.google.com/file/d/1Ms-JSv-Q3YGmoUFLYeu9u1kleXw_hJhd/view?usp=sharing')}
+              onClick={() => window.open('https://drive.google.com/file/d/1tt0whRzOs3XQLRJikPCmuY1dMFnpgi36/view?usp=sharing')}
             >
               Download CV
             </button>
